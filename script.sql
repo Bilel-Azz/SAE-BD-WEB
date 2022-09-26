eb0d2353a540e75c44fe9c78a3c5eab6ea26eccc
ARSLANHAN Emre <o22102081@info23-13.iut45.univ-orleans.fr>
2022-09-26 15:25:30 +0200

    Ajout des insertions ( à finir )

--- script.sql
+++ script.sql
@@ -25,7 +25,7 @@ CREATE TABLE COURS (
     nomcour VARCHAR(30),
     idM INT , 
     dates DATE,
-    heure TIME,
+    heure TIME check (heure > '08:00:00' and heure < '18:00:00'),
     duree TIME check (duree > '02:00:00'),
 );
 
@@ -41,6 +41,97 @@ ALTER TABLE COURS ADD FOREIGN KEY (idM) REFERENCES MONITEUR(idM);
 ALTER TABLE RESERVER ADD FOREIGN KEY (idC) REFERENCES CLIENT(idC);
 ALTER TABLE RESERVER ADD FOREIGN KEY (idPo) REFERENCES PONEY(idPo);
 ALTER TABLE RESERVER ADD FOREIGN KEY (idCour) REFERENCES COURS(idCour)
+
+
+INSERT INTO CLIENT (idC,nomC,prenomC,poidC,cotisation)
+VALUES (100,"Lazure","Arnaud",52,True),
+       (101,"Faucher","Nicolas",35,True),
+       (102,"Boivin","Helene",60,True),
+       (103,"Morin","Aubert",46,False),
+       (104,"Brisette","Madelenne",65,False);
+       (105,"Musk","Elon",52,True),
+       (106,"Tesla","Nicolas",35,True),
+       (107,"Cena","Jhon",60,True),
+       (108,"Roronoa","Zoro",46,False),
+       (109,"Uzumaki","Naruto",65,False);
+         
+
+INSERT INTO PONEY (idPo,poidP,poidSup,nomPo,agePo)
+VALUES (1,170,50,"Wolf",21),
+       (2,210,65,"Buddy",28),
+       (3,190,50,"Peak",32),
+       (4,180,50,"Poney1",28),
+       (5,200,50,"Poney2",29),
+       (6,220,50,"Poney3",26),
+       (7,230,50,"Poney4",32),
+       (8,240,50,"Poney5",38),
+       (9,250,50,"Poney6",15),
+       (10,260,50,"Poney7",25),
+       (11,270,50,"Poney8",28),
+       (12,120,50,"Poney9",12),
+       (13,290,50,"Poney10",39),
+       (14,300,50,"Poney11",25),
+       (15,310,50,"Poney12",24),
+       (16,320,50,"Poney13",23),
+       (17,196,50,"Poney14",22),
+       (18,340,50,"Poney15",18),
+       (19,100,50,"Poney16",5),
+       (20,360,50,"Poney17",29),
+       (21,385,50,"Poney18",29),
+       (22,360,50,"Poney19",27),
+       (23,320,50,"Poney20",30),
+       (24,149,50,"Poney21",17),
+       (25,186,50,"Poney22",18),
+       (26,185,50,"Poney23",20),
+       (27,216,50,"Poney24",21),
+       (28,230,50,"Poney25",22),
+       (29,124,50,"Poney26",13),
+       (30,165,50,"Poney27",27);
+
+INSERT INTO MONITEUR (idM,nomM,prenomM)
+VALUES (200,"Clavet","Fabrice"),
+       (201,"Woods","Frank"),
+       (202,"Morgan","Arthur"),
+       (203,"Clinton","Franklin"),
+       (204,"DeSanta","Michael"),
+       (205,"Phillips","Trevor"),
+       (206,"Price","Jhon"),
+       (207,"Simon","Ghoast"),
+       (208,"Makarov","Vladimir"),
+       (209,"Mason","Alex");
+
+INSERT INTO COURS (idCour,nomcour,idM,dates,heure,duree)
+values (1,"Cours1",200,"2019-01-01","08:00:00","02:00:00"),
+       (2,"Cours2",201,"2019-01-01","10:00:00","02:00:00"),
+       (3,"Cours3",202,"2019-01-01","12:00:00","02:00:00"),
+       (4,"Cours4",203,"2019-01-01","14:00:00","02:00:00"),
+       (5,"Cours5",204,"2019-01-01","16:00:00","02:00:00"),
+       (6,"Cours6",205,"2019-01-01","18:00:00","02:00:00"),
+       (7,"Cours7",206,"2019-01-01","08:00:00","02:00:00"),
+       (8,"Cours8",207,"2019-01-01","10:00:00","02:00:00"),
+       (9,"Cours9",208,"2019-01-01","12:00:00","02:00:00"),
+       (10,"Cours10",209,"2019-01-01","14:00:00","02:00:00"),
+       (11,"Cours11",200,"2019-01-01","16:00:00","02:00:00"),
+       (12,"Cours12",201,"2019-01-01","18:00:00","02:00:00"),
+       (13,"Cours13",202,"2019-01-01","08:00:00","02:00:00"),
+       (14,"Cours14",203,"2019-01-01","10:00:00","02:00:00"),
+       (15,"Cours15",204,"2019-01-01","12:00:00","02:00:00"),
+       (16,"Cours16",205,"2019-01-01","14:00:00","02:00:00"),
+       (17,"Cours17",206,"2019-01-01","16:00:00","02:00:00"),
+       (18,"Cours18",
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
