f665e9f705229a638db2f6a3304bfabdedb231d3
AAZZOUZ Bilel <o22100903@info04-05.iut45.univ-orleans.fr>
2022-12-08 11:16:58 +0100

    ajout du serv flask

---
+++ serv/app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+from sqlalchemy.sql import func
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+DATABASE_PROVIDER = "mysql"
+DATABASE_HOST = "servinfo-mariadb"
+DATABASE_USER = "aazzouz"
+DATABASE_PASS = "aazzouz"
+DATABASE_NAME = "DBaazzouz"
+SESSION_KEY_LENGTH = 24
+app.config['SQLALCHEMY_DATABASE_URI'] ="%s://%s:%s@%s/%s" % (DATABASE_PROVIDER, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
+
+
+# db = SQLAlchemy(app) 
+
+@app.route ("/")
+def home ():
+    return render_template ("home.html")
+
