318f259ccd57865cfe07c1644ba5e168c2b780af
ARSLANHAN Emre <o22102081@info25-17.iut45.univ-orleans.fr>
2023-01-11 16:30:17 +0100

    ajout de tous les url_for et ajout quelque fonctions views

--- serv/views.py
+++ serv/views.py
@@ -6,5 +6,21 @@ from flask import render_template, request, redirect, url_for
 def index():
     return render_template('index.html')
 
+@app.route ("/about")
+def about():
+    return render_template('about.html')
+
+@app.route ("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route ("/contact")
+def contact_post():
+    return redirect(url_for('contact'))
+
+@app.route ("/services")
+def services():
+    return render_template('services.html')
+
 
 
