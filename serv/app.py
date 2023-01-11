41aeaabcbf4e130758ac2af0eab1af39abe39cf9
AAZZOUZ Bilel <o22100903@info04-07.iut45.univ-orleans.fr>
2023-01-11 12:20:28 +0100

    redirection vers views

--- serv/app.py
+++ serv/app.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from core.database import db, session
-from core.Client import *
 
 
 from sqlalchemy.sql import func
@@ -17,13 +15,6 @@ DATABASE_PASS = "aazzouz"
 DATABASE_NAME = "DBaazzouz"
 SESSION_KEY_LENGTH = 24
 app.config['SQLALCHEMY_DATABASE_URI'] ="%s://%s:%s@%s/%s" % (DATABASE_PROVIDER, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
-
-
-
-
-@app.route ("/")
-def index():
-    return render_template('index.html')
 
 
 
--- serv/views.py
+++ serv/views.py
@@ -1,3 +1,10 @@
 from .app import app
 from flask import render_template, request, redirect, url_for
 
+
+@app.route ("/")
+def index():
+    return render_template('index.html')
+
+
+
