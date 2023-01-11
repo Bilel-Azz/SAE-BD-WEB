406cbe5490f8353e3ad62e1ae75d24588f9c0a5b
AAZZOUZ Bilel <o22100903@info04-07.iut45.univ-orleans.fr>
2023-01-11 12:06:31 +0100

    modif

--- serv/app.py
+++ serv/app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from core.database import db, session
+from core.Client import *
 
 
 from sqlalchemy.sql import func
@@ -9,7 +11,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 DATABASE_PROVIDER = "mysql"
-DATABASE_HOST = "servinfo-mariadb"
+DATABASE_HOST = "servinfmariadb"
 DATABASE_USER = "aazzouz"
 DATABASE_PASS = "aazzouz"
 DATABASE_NAME = "DBaazzouz"
@@ -17,9 +19,11 @@ SESSION_KEY_LENGTH = 24
 app.config['SQLALCHEMY_DATABASE_URI'] ="%s://%s:%s@%s/%s" % (DATABASE_PROVIDER, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
 
 
-# db = SQLAlchemy(app) 
+
 
 @app.route ("/")
-def home ():
-    return render_template ("home.html")
+def index():
+    return render_template('index.html')
+
+
 
