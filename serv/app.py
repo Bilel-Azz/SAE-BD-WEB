0ab7bd5f148c0cb210f1867888e9c7aaddde5ee6
ARSLANHAN Emre <o22102081@info26-13.iut45.univ-orleans.fr>
2023-01-16 14:45:14 +0100

    pour push

--- serv/app.py
+++ serv/app.py
@@ -12,10 +12,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 DATABASE_PROVIDER = "mysql"
-DATABASE_HOST = "localhost"
-DATABASE_USER = "root"
-DATABASE_PASS = "root"
-DATABASE_NAME = "dbarslanhan"
+DATABASE_HOST = "servinfmariadb"
+DATABASE_USER = "arslanhan"
+DATABASE_PASS = "arslanhan"
+DATABASE_NAME = "DBarslanhan"
 SESSION_KEY_LENGTH = 24
 app.config['SQLALCHEMY_DATABASE_URI'] ="%s://%s:%s@%s/%s" % (DATABASE_PROVIDER, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
 app.secret_key = urandom(SESSION_KEY_LENGTH)
